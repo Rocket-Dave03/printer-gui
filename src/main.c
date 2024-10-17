f706ec4f972eaef7e3d4266f4a6ee318999bac57
Rocket_dave03 <rocketdave30@gmail.com>
2024-10-17 13:46:34 -0300

    Horizontal layout testing

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "buffer.h"
 #include "gui/gui_box.h"
 #include "gui/layout/gui_layout_vertical.h"
+#include "gui/layout/gui_layout_horizontal.h"
 #include "pixel.h"
 #include "gui/gui_element.h"
 
@@ -23,18 +24,33 @@ int main() {
 
 	struct GuiElement *box = gui_box_create(NULL,(struct Pixel){51,51,51,255});
 	gui_layout_vertical_create(box);
+
+	struct GuiElement *row_1 = gui_box_create(box, (struct Pixel){150,100,100,255});
+	gui_layout_horizontal_create(row_1);
+	gui_box_create(row_1, (struct Pixel){150, 50, 50,255});
+	gui_box_create(row_1, (struct Pixel){150, 90, 90,255});
+	gui_box_create(row_1, (struct Pixel){150,120,120,255});
+
+	struct GuiElement *row_2 = gui_box_create(box, (struct Pixel){100,150,100,255});
+	gui_layout_horizontal_create(row_2);
+	gui_box_create(row_2, (struct Pixel){ 50,150, 50,255});
+	gui_box_create(row_2, (struct Pixel){ 90,150, 90,255});
+	gui_box_create(row_2, (struct Pixel){120,150,120,255});
+
+	struct GuiElement *row_3 = gui_box_create(box, (struct Pixel){100,100,150,255});
+	gui_layout_horizontal_create(row_3);
+	gui_box_create(row_3, (struct Pixel){ 50, 50,150,255});
+	gui_box_create(row_3, (struct Pixel){ 90, 90,150,255});
+	gui_box_create(row_3, (struct Pixel){120,120,150,255});
+
 	gui_propogate_update(box, GUI_UPDATE_STATIC);
-	gui_box_create(box, (struct Pixel){100,150,100,255});
-	gui_box_create(box, (struct Pixel){150,100,100,255});
-	gui_box_create(box, (struct Pixel){100,100,150,255});
-
-
 	while (1) {
 		fill_buffer(render_buffer, (struct Pixel){0,0,0,255});
 		gui_show(box, render_buffer, 0, 0);
 		
 		stbi_write_png("/run/user/1000/test.png", render_buffer->width, render_buffer->height, 4, (render_buffer->pixels), render_buffer->width * sizeof(struct Pixel));
 		usleep(1000 * 1000/60);
+		break;
 	}
 	gui_delete_element(box);
 	delete_buffer(render_buffer);
